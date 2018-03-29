02148bab300fb94d434a9d0aaf5ff0774963f933
梁木俊冴 <toshiki.dentu.1226@gmail.com>
2018-03-29 10:09:05 +0900

    2017/3/29

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,8 +1,7 @@
 describe('LearnJS', function() {
     it('can show a proble, view', function(){
 	learnjs.showView('#problem-1');
-	expect($('.view-container .problem-view').length).toEqual(1);
-	
+	expect(0).toEqual(1);
     });
     
 });
